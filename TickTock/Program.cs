01a8e2abea4b06ef09d5b47214838321159053c9
Sławomir Brzozowski <brzozasr@interia.eu>
2021-03-09 23:22:02 +0100

    Init commit.

---
+++ TickTock/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TickTock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
